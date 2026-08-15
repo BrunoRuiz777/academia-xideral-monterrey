381d4dac62111e0990563a86dbafc8b151cf4a2c
BrunoRuiz777 <cbm0100608ruiz@gmail.com>
2026-08-15 03:58:32 -0600

    feat: integrate AttackStrategy into GameCharacter using composition

--- src/entities/GameCharacter.java
+++ src/entities/GameCharacter.java
@@ -1,5 +1,7 @@
 package entities;
 
+import combat.AttackStrategy;
+
 // clase abstracta personaje del juego que implementa la interface atacable
 public abstract class GameCharacter implements Attackable {
 
@@ -7,12 +9,23 @@ public abstract class GameCharacter implements Attackable {
     private String name;
     protected int health;
     private int maxHealth;
+    protected AttackStrategy currentWeapon;
 
     // constructor cuando creamos un personaje debemos definirle un nombre y vida maxima
     public GameCharacter(String name, int maxHealth) {
         this.name = name;
         this.maxHealth = maxHealth;
         this.health = maxHealth;
+    }
+
+    // el personaje ataca usando el arma que tenga equipada, si no tiene arma equipada para evitar un error NullPointerException y pega con las manos
+    public int attack() {
+        if (this.currentWeapon != null) {
+            return this.currentWeapon.calculateDamage();
+        } else {
+            System.out.println(this.getName() + " no tiene un arma equipada y ataca con los puños.");
+            return 1; // Daño mínimo por golpear sin arma
+        }
     }
 
     // metodo con logica sobre escribimos el comportamiento de tomar daño con override y con la logica del if evitamos que tenga vida negativa
@@ -22,6 +35,7 @@ public abstract class GameCharacter implements Attackable {
         if (this.health < 0) {
             this.health = 0;
         }
+
     }
 
     // dejamos este metodo abstracto para que cada personaje pueda definir como celebrar su vistoria, no retorna nada, es publico.
@@ -31,4 +45,8 @@ public abstract class GameCharacter implements Attackable {
     public String getName() {return this.name;}
     public int getHealth() {return this.health;}
     public int getMaxHealth() {return this.maxHealth;}
+
+    public void setCurrentWeapon(AttackStrategy currentWeapon) {
+        this.currentWeapon = currentWeapon;
+    }
 }
